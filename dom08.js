552044b21da57f390e02a984c241c1516a976ae8
Marcos Andrade <mvpdeandrade@gmail.com>
2022-07-12 21:03:07 -0300

    refatora listeners para arrow

--- dom08.js
+++ dom08.js
@@ -2,29 +2,29 @@ const eValor = document.querySelector("p");
 
 const eButtons = document.querySelectorAll("button");
 const eParButton = eButtons[0];
-eParButton.addEventListener("click", ehParListener);
 const eImparButton = eButtons[1];
-eImparButton.addEventListener("click", ehImparListener);
 
 const eLists = document.querySelectorAll("ul");
 const eParList = eLists[0];
 const eImparList = eLists[1];
 
-function ehParListener(event){
+
+eParButton.addEventListener("click", function (event) {
     console.log("Eh par!")
     criaElemento(eParList);
-}
-function ehImparListener(event){
+});
+eImparButton.addEventListener("click",  (event)=> {//quase mesma coisa que function(event)
     console.log("Eh Impar!")
     criaElemento(eImparList);
+});
+
+function sorteiaValor() {
+    const novoNumero = Math.floor(Math.random() * 10);
+    eValor.textContent = novoNumero;
 }
-function criaElemento(ehPar){
+function criaElemento(lista) {
     const eLi = document.createElement("li");
     eLi.textContent = eValor.textContent;
     lista.appendChild(eLi);
     sorteiaValor();
 }
-function sorteiaValor() {
-    const novoNumero = Math.floor(Math.random()*10);
-    eValor.textContent = novoNumero;
-}    
