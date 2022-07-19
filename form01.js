b3f06d9c77e4c9afddcca17d03f48d6b09a224bb
Marcos Andrade <07984519633@estudante.ufjf.br>
2022-07-19 20:29:08 -0300

    adiciona evento no submit

--- form01.js
+++ form01.js
@@ -1,10 +1,17 @@
-function criaElemento(cor, largura, altura){
+document.exm01.addEventListener('submit', submitListener);
+
+function criaElemento(cor, altura,largura){
     const eNovo = document.createElement("div");
-    eNovo.textoContent = `${cor} ${largura} ${altura}`;
+    eNovo.textContent = `${cor} ${altura} ${largura}`;
     document.body.appendChild(eNovo);
 }
-criaElemento(
-    document.exm01.cor.value,
-    document.exm01.largura.value,
-    document.exm01.altura.value
-    );
+
+function submitListener(event) {
+    event.preventDefault();
+
+    criaElemento(
+        document.exm01.cor.value,
+        document.exm01.largura.value,
+        document.exm01.altura.value
+        );
+}
