4acafb4c1f27c49063e5c7252eda7e982e581e4a
Marcos Andrade <07984519633@estudante.ufjf.br>
2022-07-19 20:58:13 -0300

    fiquei sem criatividade

--- form01.js
+++ form01.js
@@ -1,19 +1,25 @@
+let estado ={
+cor: "#000000",
+largura:0,
+altura:0
+};
+
 document.exm01.addEventListener('submit', submitListener);
 
 function criaElemento(cor, altura,largura){
     const eNovo = document.createElement("div");
-    eNovo.textContent = `${cor} ${altura} ${largura}`;
+    eNovo.textContent = `${cor} ${altura}  ${largura} ${altura+largura}`;
+    eNovo.style=`color: ${cor};`
     document.body.appendChild(eNovo);
 }
 
 function submitListener(event) {
     event.preventDefault();
-
-    criaElemento(
-        document.exm01.cor.value,
-        document.exm01.altura.value,
-        document.exm01.largura.value
-        );
+    estado.cor = document.exm01.cor.value,
+    estado.altura = Number(document.exm01.altura.value),
+    estado.largura = Number(document.exm01.largura.value) 
+    criaElemento(estado.cor,
+    estado.largura,estado.altura);
 
  document.exm01.reset();
 }
